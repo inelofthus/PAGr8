ae5a9edb8733463d69b1345e5306ad2ff86002dd
jonasbgi <jonasbgi@stud.ntnu.no>
2018-03-07 18:35:32 +0100

    Added components from architecture

---
+++ core/src/com/tdt4240/jankenlabyrinth/gameecs/components/LocalPlayer.java
@@ -0,0 +1,11 @@
+package com.tdt4240.jankenlabyrinth.gameecs.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by jonas on 07/03/2018.
+ */
+
+public class LocalPlayer implements Component {
+    //Life begins with an empty slate. Sadly this component never fills it's slate either
+}
