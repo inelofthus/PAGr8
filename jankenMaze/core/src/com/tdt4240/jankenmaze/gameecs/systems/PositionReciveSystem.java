6e335f2acd926f8d4afa5e8f2651a82ca82f5ee0
Bartosz <bartosz.zarosa@hotmail.com>
2018-04-10 13:42:04 +0200

    Made PositionReciveSystem

---
+++ jankenMaze/core/src/com/tdt4240/jankenmaze/gameecs/systems/PositionReciveSystem.java
@@ -0,0 +1,23 @@
+package com.tdt4240.jankenmaze.gameecs.systems;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.EntitySystem;
+
+public class PositionReciveSystem extends EntitySystem {
+
+    public PositionReciveSystem(){
+
+    }
+
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+        super.removedFromEngine(engine);
+    }
+
+}
