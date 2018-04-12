946ff47969625c0c0fe7d0e6e823df2e99d78116
thaiKari <karimelingjohannessen@gmail.com>
2018-04-12 13:17:59 +0200

    began creating HealthMessage Singleton class

---
+++ jankenMaze/core/src/com/tdt4240/jankenmaze/gameMessages/HealthMessage.java
@@ -0,0 +1,9 @@
+package com.tdt4240.jankenmaze.gameMessages;
+
+/**
+ * Created by karim on 12/04/2018.
+ */
+
+public class HealthMessage {
+    public static final HealthMessage INSTANCE = new HealthMessage();
+}
